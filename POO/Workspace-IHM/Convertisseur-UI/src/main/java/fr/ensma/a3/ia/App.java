1f490b630c86e6a5696a663dbfafd9c1c954dc0a
OnlyLogiicC <moi.martinperez@gmail.com>
2021-11-27 13:55:44 +0100

    Progression TD1 JavaFX

--- POO/Workspace-IHM/Convertisseur-UI/src/main/java/fr/ensma/a3/ia/App.java
+++ POO/Workspace-IHM/Convertisseur-UI/src/main/java/fr/ensma/a3/ia/App.java
@@ -1,51 +1,26 @@
 package fr.ensma.a3.ia;
 
 import javafx.application.Application;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-	
-	private Scene scene ;
-	private FlowPane root ;
-	private VBox box_button ;
-	private Button btn ;
-	private ComboBox<String> cbox ;
-	private TextField txtf_left ;
-	private TextField txtf_right ;
-	
+
     @Override
-    public void start(Stage primaryStage) {
-    	primaryStage.setTitle("Covertisseur Euro<->Dollars") ;
-    	root = new FlowPane(Orientation.HORIZONTAL,10,10) ;
-    	root.setAlignment(Pos.TOP_CENTER) ;
-    	txtf_left = new TextField() ;
-    	box_button = new VBox(20) ;
-    	box_button.setAlignment(Pos.TOP_CENTER);
-    	cbox = new ComboBox<>() ;
-    	cbox.getItems().addAll("Dollars->Euros","Euros->Dollars") ;
-    	cbox.getSelectionModel().select(0) ;
-    	box_button.getChildren().add(cbox) ;
-       	btn = new Button("Convertir") ;
-       	box_button.getChildren().add(btn) ;
-       	txtf_right = new TextField() ;
-       	root.getChildren().addAll(txtf_left, box_button, txtf_right) ;
-       	scene = new Scene(root, 200, 100) ;
-       	primaryStage.setMinHeight(130) ;
-       	primaryStage.setMinWidth(700);
-       	primaryStage.setScene(scene) ;
-       	primaryStage.show() ;
-    	
-        
+    public void start(Stage stage) {
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
